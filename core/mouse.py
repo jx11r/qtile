8e25865fdca3a7d252fd3819aaf623ae05488e5b
jx11r <jx11r@hotmail.com>
2022-04-08 20:25:31 -0500

    Update mouse.py

--- core/mouse.py
+++ core/mouse.py
@@ -11,18 +11,21 @@ mouse = [
         [mod],
         'Button1',
         lazy.window.set_position_floating(),
-        start = lazy.window.get_position()),
+        start = lazy.window.get_position(),
+    ),
 
     # Right click
     Drag(
         [mod],
         'Button3',
         lazy.window.set_size_floating(),
-        start = lazy.window.get_size()),
+        start = lazy.window.get_size(),
+    ),
 
-    # Wheel
+    # Scroll wheel
     Click(
         [mod],
         'Button2',
-        lazy.window.bring_to_front()),
+        lazy.window.bring_to_front(),
+    ),
 ]

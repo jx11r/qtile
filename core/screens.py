770300cfe058fe4767566070caaa20a1c5501486
jx11r <jx11r@hotmail.com>
2022-05-21 01:48:50 -0500

    Add new function

--- core/screens.py
+++ core/screens.py
@@ -4,6 +4,16 @@ from libqtile import bar
 from libqtile.config import Screen
 
 wallpaper = '~/wallpapers/wp1.png'
+screen_height = 768
+
+def calculate(height: int) -> dict:
+    size = height * 25 / 1080
+    border = height * 6 / 1080
+
+    return {
+        'size': round(size),
+        'border': round(border),
+    }
 
 screens = [
     Screen(

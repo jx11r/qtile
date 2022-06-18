8e70c14be82ac2cd3df6ab21e4a904e4d10ddd93
jx11r <jx11r@hotmail.com>
2022-06-18 00:45:20 -0500

    utils: add dir module

---
+++ utils/dir.py
@@ -0,0 +1,10 @@
+from subprocess import Popen, PIPE
+
+def get() -> str:
+    process = Popen(
+        ['pwd'],
+        stdout = PIPE,
+        text = True,
+    )
+
+    return process.communicate()[0]

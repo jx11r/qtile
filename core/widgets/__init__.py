425439c105128a037e92eb0b365a7f73fbda0829
jx11r <jx11r@hotmail.com>
2022-06-19 21:12:03 -0500

    core: import widgets dynamically

---
+++ core/widgets/__init__.py
@@ -0,0 +1,8 @@
+from core.widgets.bar import config
+
+tag, bar = config()
+
+__all__ = [
+    'tag',
+    'bar'
+]
---
+++ core/widgets/bar.py
@@ -0,0 +1,14 @@
+from importlib import import_module
+
+BAR: str = 'rounded'
+
+def config() -> tuple[list, dict]:
+    try:
+        module = import_module(f'core.widgets.{BAR}')
+        module.bar.update(
+            { 'widgets': module.widgets }
+        )
+    except ImportError:
+        return [], {}
+
+    return module.tag, module.bar

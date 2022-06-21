6f7b451f3d5e592032069a61abdb1ead3bee90b1
jx11r <jx11r@hotmail.com>
2022-06-21 11:49:23 -0500

    init: add qtile extras

--- extras/__init__.py
+++ extras/__init__.py
@@ -1,5 +1,17 @@
+from qtile_extras import widget
+from qtile_extras.widget import modify
+from qtile_extras.popup import toolkit
+from qtile_extras.widget.decorations import (
+    BorderDecoration, RectDecoration
+)
+
 from extras.groupbox import GroupBox
 
 __all__ = [
+    'BorderDecoration',
     'GroupBox',
+    'modify',
+    'RectDecoration',
+    'toolkit',
+    'widget',
 ]

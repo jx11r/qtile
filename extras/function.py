bb6d138d45e98cde36bf7ba0f136537c5dbc6714
jx11r <jx11r@hotmail.com>
2022-10-14 22:54:36 -0500

    extras: fix deprecated function

--- extras/function.py
+++ extras/function.py
@@ -3,4 +3,4 @@ from libqtile.core.manager import Qtile
 def float_to_front(qtile: Qtile) -> None:
   for window in qtile.current_group.windows:
     if window.floating:
-      window.cmd_bring_to_front()
+      window.bring_to_front()

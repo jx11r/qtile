7ac8d802db66ce236c77cb817983839e48ce4054
jx11r <jx11r@hotmail.com>
2022-10-22 19:29:33 -0500

    bar: set wm_name at startup

--- core/hooks.py
+++ core/hooks.py
@@ -1,6 +1,20 @@
 import asyncio
 from libqtile import hook
 
+from core.bar import bar
+
+margin = sum(bar.margin) if bar else -1
+
+@hook.subscribe.startup
+def startup():
+  if margin == 0:
+    bar.window.window.set_property(
+      name = 'WM_NAME',
+      value = 'QTILE_BAR',
+      type = 'STRING',
+      format = 8,
+    )
+
 @hook.subscribe.client_new
 async def client_new(client):
   await asyncio.sleep(0.5)

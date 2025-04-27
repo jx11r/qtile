98447412b819caa230a65262f0ad2a4226efe574
jx11r <jx11r@hotmail.com>
2025-04-27 14:49:05 -0600

    groups: add more workspaces

--- core/groups.py
+++ core/groups.py
@@ -7,12 +7,14 @@ from utils.match import wm_class
 groups: list[Group] = []
 
 for key, label, layout, matches in [
-    ("1", "", None, wm_class()),
-    ("2", "", "max", wm_class("code")),
-    ("3", "", None, wm_class("insomnia", "obs", "evince")),
+    ("1", "", "monadtall", wm_class()),
+    ("2", "", "monadtall", wm_class("code")),
+    ("3", "󰆼", "monadwide", wm_class("insomnia")),
+    ("4", "󰹍", "max", wm_class("obs", "gimp-2.10")),
     ("q", "󰈹", "max", wm_class("brave-browser", "firefox")),
-    ("w", "󰇮", "max", wm_class("discord", "telegram-desktop", "zapzap")),
-    ("f", "", "max", wm_class("cider", "vlc")),
+    ("w", "󰇮", "monadwide", wm_class("discord", "telegram-desktop")),
+    ("f", "󰝰", "monadtall", wm_class("evince")),
+    ("p", "", "max", wm_class("cider", "vlc")),
 ]:
     groups.append(Group(key, matches, label=label, layout=layout))
 

b6c02c509f182e1ae330dbfd93aa41b99aceddfc
jx11r <git@jx11r.dev>
2025-09-14 17:42:17 -0600

    fix(groups): update matches

--- core/groups.py
+++ core/groups.py
@@ -10,9 +10,9 @@ for key, label, layout, matches in [
     ("1", "", "monadtall", wm_class()),
     ("2", "", "monadtall", wm_class("code")),
     ("3", "󰆼", "monadwide", wm_class("insomnia")),
-    ("4", "󰹍", "max", wm_class("obs", "gimp-2.10")),
+    ("4", "󰹍", "max", wm_class("obs", "gimp")),
     ("q", "󰈹", "max", wm_class("brave-browser", "firefox")),
-    ("w", "󰇮", "monadwide", wm_class("discord", "telegram-desktop")),
+    ("w", "󰇮", "monadtall", wm_class("discord", "Telegram")),
     ("f", "󰝰", "monadtall", wm_class("Evince")),
     ("p", "", "max", wm_class("cider", "vlc")),
 ]:
